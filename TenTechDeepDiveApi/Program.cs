eaf3f460dcf5cdf9042278bc89c7204cf8d9b9a9
rajivpandey67 <rajivpandey67@gmail.com>
2025-03-23 14:32:03 +0530

    Removed unnecessary piece

--- TenTechDeepDiveApi/Program.cs
+++ TenTechDeepDiveApi/Program.cs
@@ -14,7 +14,6 @@ namespace TenTechDeepDiveApi
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
